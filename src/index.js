a86c999a582400ccb0e56f2e4d33b3b5bd83ba4c
Kyle Olson <kaollie74@gmail.com>
2019-07-22 09:08:25 -0500

    cleaned up some code

--- src/index.js
+++ src/index.js
@@ -11,61 +11,6 @@ import logger from 'redux-logger';
 import {Provider} from 'react-redux';
 
 
-// const feedback = [
-//     {feeling: 0, understanding: 0 , support: 0, comments: ''}
-// ]
-
-// const feelingReducer = (state= {}, action) => {
-       
-        
-//     if(action.type === 'UPDATE_FEELING'){
-//         return [action.payload];
-//     }
-//     return state;
-// }
-
-// const understandingReducer = (state= {}, action) => {
-//     if(action.type === 'UPDATE_UNDERSTANDING' ){
-//         return [action.payload]
-//     }
-//     return state;
-// }
-
-// const supportReducer = (state = {}, action ) => {
-//     if(action.type === 'UPDATE_SUPPORT'){
-//         return [action.payload]
-//     }
-//     return state;
-// }
-
-// const commentReducer = (state = {}, action) => {
-//     if (action.type === 'UPDATE_COMMENT') {
-//         return [action.payload]
-//     }
-//     return state;
-// }
-
-
-// const feedbackSubmit = [ {feeling: '', understanding: '', support: '', comment: ''}]
-
-
-// const feedbackReducer = (state = {}, action) => {
- 
-//     if (action.type === 'UPDATE_FEELING'){
-//         console.log('in update feeling', action.payload)
-//        return [action.payload]
-//     } else if (action.type === 'UPDATE_UNDERSTANDING'){
-//        return [action.payload]
-//     }else if(action.type === 'UPDATE_SUPPORT'){
-//       return [action.payload]
-//     }else if (action.type === 'UPDATE_COMMENT'){
-//        return [action.payload]
-//     }
-//     console.log('In feedback reducer', state);
-    
-//     return state;
-// }
-
 const feedbackReducer = (state = {feeling: 0, understand: 0, support: 0, comment: ''}, action) => {
     console.log('in survey reducer, payload is: ', action.payload)
     if (action.type === 'UPDATE_FEELING') {
